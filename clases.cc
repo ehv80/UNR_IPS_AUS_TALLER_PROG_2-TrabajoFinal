7e5293daf4df51c9ce910ba272df2b6232928317
Ezequiel Hernán Villanueva <ezequiel.villanueva@gmail.com>
2021-02-04 12:04:08 -0300

    Add files via upload

--- clases.cc
+++ clases.cc
@@ -1,169 +1,53 @@
-#include <iostream>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <string.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <glob.h>
-
-using namespace std;
-
-#include "clases.hpp"
-
-#define OPC1    (GLOB_TILDE|GLOB_NOCHECK)
-#define OPC2    (OPC1|GLOB_APPEND)
-
-#define FORK(x)         ({ int i=fork(); cerr<<x<<" "<<i<<'\n'; i; })
-
-static void error( const char * s , int err )
-{
-	char b[MAX_CANON];
-	strerror_r( err , b , sizeof(b) );
-	cerr << s << ": " << b << '\n';
-}
-
-static void glob_error( int error )
-{
-	cerr << "glob: ";
-	switch(err) {
-		case GLOB_NOSPACE:
-			cerr << "nospace!"; break;
-		case GLOB_ABORTED:
-			cerr << "aborted!"; break;
-		case NO_MATCH:
-			cerr << "nomatch!"; break;
-		default:
-			cerr << "oops!";
-	}
-	cerr << '\n';
-}
-				
-int Cd::exec( enum modo ) {
-	glob_t g;
-	int r;
-	if( ( r = glob( camino, GLOB_NOCHECK|GLOB_TILDE, NULL, &g)))
-		glob_error(r);
-	if( chdir( g.gl_path[0]) )
-		error("cd", errno);
-	return 0;
-}
-
-char * Comando::path( char * p )
-{
-	char * tmp = new char[1024];
-	if(strchr(p, '/')!=0) {
-		strcpy(tmp, p);
+char * Comando : : path (char * p)
+{	
+	char * tmp = new char[ 1024 ];
+	if( strchr( p , '/' ) != 0 ) {
+		strcpy( tmp , p );
 		return tmp;
 	}
-	char env[1024];
-	strcpy(env, getenv("PATH"));
-	char *pc=strtok(env, ":");
-	do {
-		strcat(strcat(strcpy(tmp, pc), "/"), p);
-		if(access(tmp, X_OK)==0)
-			return tmp;
-	} while((pc=strtok(0, ":"))!=0);
-	return p;
-}
+	char env[ 1024 ];
+		strcpy( env , getenv("PATH"));
+		char * pc = strtok( env , ":");
+	do{
+		strcat( strcat ( strcpy( tmp , pc ) , "/" ) , p);
+		if( acces( tmp, X_OK ) == 0 )
+		return tmp;
+	} while ( ( pc = strtok( NULL, ":" )) != 0 );
+		rerturn p;
+};
 
-Comando::~Comando() {
-	delete prog;
-	int i;
-	for( i=0; args[i]; i++)
-		delete args[i];
-	delete args;
-}
-
-int Comando::exec(enum modo m)
+// hay que agregar a los prototipos
+// ver:	 man fun.c
+// ver:	 man string.h
+// continúa código
+int Comando : : exec( enum modo m )
 {
-	programa = path( programa );
-	if(m == __Fork && FORK("Comando") !=0 ) {
-		int status;
-		wait(&status);
-		return status;
-	}
-	glob_t pg;
-	int r;
-	if((r=glob(argumentos[0], OPC1, NULL, &pg)))
-		glob_error(r);
-	int i;
-	for( i = 1; argumentos[i]; i++)
-		if( ( r = glob( argumentos[i], OPC2, NULL, &pg ) ) )
-			glob_error(r);
-	execv( programa, pg.gl_pathv );
-	exit(-1);
-	return 0;
-}
-
-int Pipe::exec(enum modo m)
-{
-	int pip[2];
-	if(m == __Fork && fork()!= 0 )
-		wait(NULL);
-	else {
-		if( pipe(pip) ) {
-			error( "pipe", errno );
-			exit(-1);
-		}
-		if( FORK( "Pipe 2" ) != 0 ) {
-			dup2( pip[1] , 1 );
-			close( pip[0] );
-			close( pip[1] );
-			return i -> exec(__Exec);
-		}
-		else {
-			// completar
-			return d -> exec(__Exec);
-		}
-		exit( -1 );
-	}
-	return 0;
-}
-
-int RedireccionEntrada::exec(enum modo m)
-{
-	// completar
-	return 0;
-}
-         
-int RedireccionSalida::exec(enum modo m)
-{
-	// completar
-	return 0;
-}
-         
-int And::exec(enum modo)
-{
+	if( m == __Fork && fork() != 0 ) {
 	int status;
-	if( FORK( "And 1" ) !=0 ) wait(&status);
-	else return izquierda -> exec( __Exec );
-	if( !WIFEXITED( status ) || WEXITSTATUS( status ) !=0 )
-		return -1;
-	if( FORK( "And 2" ) !=0 ) wait( &status );
-	else return derecha -> exec( __Exec );
-	if( WIFEXITED( status ) )
-		return WEXITSTATUS( status );
-	return -1;
-}
-
-int Or::exec(enum modo)
-{
-	// completar
-	return -1;
-}
-
-
-int Secuencia::exec(enum modo m)
-{
-	// completar
-	return -1;
-}
-
-
-int SubShell::exec(enum modo)
-{
-	// completar
+	wait( &status );
 	return status;
+}
+	glob_k pg;
+	int r;
+	if( ( r = glob( arg[ 0 ] , GLOB_TILDE | GLOB_NOCHECK , NULL, &pg ) ) )
+	glob_error( r );
+	for( int i=1 ; arg[ i ] , i++ )
+	if(( r = glob( args[i], GLOB_TILDE | GLOB_NOCHECK | GLOB_APPEND ,NULL, &pg) ) )
+		glob.error( r );
+	execv( prog , pg.gl_pathv );
+	exit( -1 );
+	return 0;
+	
+// main de ejemplo
+
+int main()
+{
+	char ** args = {
+		"ls" , "*.c*" , "a*" , NULL
+	}
+	Comando c("ls" , args );
+	c.exec( __Exec );
+	return 0;
 }
--- clases.hpp
+++ clases.hpp
@@ -1,101 +1,35 @@
-#include <iostream>
-
-using namespace std;
-
-enum modo { __Fork, __Exec };
-
-/* CLASE ABSTRACTA */
+#include <iostream.h>
+enum modo { __Fork, __Exec }
 class Accion {
 	public: 
-		virtual ~Accion(){ }	/* destructor */
-		virtual int exec( enum modo) = 0;
+		virtual ~Accion(){ }
+		virtual int exec( enum modo) =0;
 };
 
-// concretas que heredan de Accion
+// concretas
 
 class Cd : public Accion {
- 	char * camino;
+ 	char * path;
  	public:
- 		Cd( char * camino ): camino(camino) {}	/* constructor */
- 		~Cd() { delete camino; }	/* destructor */
- 		int exec( enum modo ); 		/*{ chdir( path ); }*/
+ 		Cd( char * path ) : path( path ) {}
+ 		int exec( enum modo ) { chdir( path ); }
+ 		~Cd() { delete path; }
 };
 
 class Pwd : public Accion {
 	public:
-		int exec( enum modo ) { cout << get_current_dir_name << '\n' ; return 0; }
+	int exec( enum modo ) { cout << get_current_dir_name << '\n' ; return 0; }
 };
 
-class Exit : public Accion {
-	public:
-		int exec(enum modo) { exit(0); return 0; }
-};
 
 class Comando : public Accion {
-		char *programa ,  **argumentos;
+		char *prog ,  **args;
  		static char * path( char * );
 public:
-	Comando( char * programa , char ** argumentos) : programa(programa), argumentos(argumentos) {}	/* constructor */
- 	~Comando();  			/* destructor */
-	int exec( enum modo );	/*{ excecl( path( prog ) , args );*/
-};
-
-class And: public Accion {
-	        Accion * izquierda, * derecha;
-	public:
-		And(Accion * izquierda, Accion * derecha) : izquierda(izquierda), derecha(derecha) {}
-		~And() { delete izquierda; delete derecha; }
-		int exec(enum modo);
-};
-
-class Or : public Accion {
-		Accion *izquierda, *derecha;
-	public:
-	        Or(Accion * izquierda, Accion *derecha) : izquierda(izquierda), derecha(derecha) {}
-		~Or() { delete izquierda; delete derecha; }
-	        int exec(enum modo);
-};
-
-class Pipe: public Accion {
-		Accion * izquierda, * derecha;
-	public:
-		Pipe(Accion * izquierda, Accion * derecha): izquierda(izquierda), derecha(derecha) {}
-		~Pipe() { delete izquierda; delete derecha; }
-		int exec(enum modo);
-};
-
-class RedireccionEntrada : public Accion {
-		Accion * paccion;
-		char * parchivo;
-	public:
-		RedireccionEntrada( Accion * paccion , char * parchivo) : paccion(paccion), parchivo(parchivo) {}
-		~RedireccionEntrada() { delete paccion; delete parchivo; }
-		int exec(enum modo);
-};
-
-class RedireccionSalida : public Accion {
-	Accion * paccion;
-	char * parchivo;
-	bool append;
-public:
-RedireccionSalida(Accion * paccion, char * parchivo, bool append) : paccion(paccion), parchivo(parchivo), append(append) {}
-	~RedireccionSalida() { delete paccion; delete parchivo; }
-	int exec(enum modo);
-};
-
-class Secuencia : public Accion {
-	        Accion * izquierda, * derecha;
-	public:
-		Secuencia(Accion * izquierda, Accion * derecha ) : izquierda(izquierda), derecha(derecha) {}
-		~Secuencia() { delete izquierda; delete derecha; }
-		int exec(enum modo);
-};
-
-class SubShell: public Accion {
-		Accion * accion;
-	public:
-	        SubShell(Accion * accion): accion(accion) {}
-	        ~Subshell() { delete accion; }
-	        int exec(enum modo);
+	Comando( char * p , char ** a) : prog ( path( p ) ), args(a) {}
+ 	~Comando() { delete prog ; delete args; }
+// Ejercicio: acá hay un problema encontrarlo y subsanarlo
+	int exec( enum modo);
+	
 };
 
